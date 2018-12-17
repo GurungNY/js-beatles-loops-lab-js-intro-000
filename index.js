fa8b9d60eb1f9c838dd3d9512ff6d4721f6ace28
Yosuf Gurung <yosufgurung@gmail.com>
2018-12-17 05:07:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 
-function johnLennonFacts(array) {
-  console.log array(facts[] + '!!!');
-//  array = facts[] + '!!!'
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]; 
+function johnLennonFacts(facts) {
+  const newFacts = []
 
-  return array[]
+  let i = 0
+  while (i < facts.length) {
+    newFacts.push('${facts[i]}!!!') i ++
+  }
+
+  return newFacts[]
 }
